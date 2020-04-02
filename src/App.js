7977c231b0ffb88cbb02c52aef6674bd91f9422c
gnapiorkowski <gustaw.napiorkowski@gmail.com>
2020-04-02 16:05:38 +0200

    added: production build

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-	  Hello, this is a feature!
+	  Hello, this is a feature! Noice job!
         </p>
         <a
           className="App-link"
